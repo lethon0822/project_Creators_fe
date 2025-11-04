5e51c15f5e750b93a73d38af76585817d64b5b41
1ethon0822 <1ethyjj9240@gmail.com>
2025-11-04 13:36:56 +0900

    header 작업중

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { reactive } from 'vue';
+import logo from '@/assets/logo.png'
 
 </script>
 
 <template>
-
+    <img :src="logo" alt="로고 이미지">
 </template>
 
 <style scoped>

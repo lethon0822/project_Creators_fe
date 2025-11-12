be3aa46c84530de30d7a67b5fe25cdd6e9637540
1ethon0822 <1ethyjj9240@gmail.com>
2025-11-12 12:31:07 +0900

    로그인 화면중

---
+++ src/components/common/Login.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <body>
+    <div class="title">Login</div>
+    <div class="container">
+      <div class="login-box">
+        <div></div>
+      </div>
+    </div>
+  </body>
+</template>
+
+<style scoped>
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 70px 0 20px;
+  font-size: 24px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 70px;
+  height: 100%;
+}
+
+.login-box {
+  width: 500px;
+  height: 300px;
+  border: 1px solid #000;
+  border-radius: 20px;
+  background-color: #f6f6f6;
+}
+</style>
